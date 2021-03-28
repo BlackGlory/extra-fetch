29fb64de20e8df69e1da3d930bd89c634f882edf
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-28 04:34:35 +0000

    test: add cases

---
+++ __tests__/node/request.spec.ts
@@ -0,0 +1,17 @@
+import { Request } from '@src/utils/request'
+
+describe('Request', () => {
+  describe('keepalive', () => {
+    test('default', () => {
+      const req = new Request('http://localhost')
+
+      expect(req.keepalive).toBe(false)
+    })
+
+    test('set', () => {
+      const req = new Request('http://localhost', { keepalive: true })
+
+      expect(req.keepalive).toBe(true)
+    })
+  })
+})
