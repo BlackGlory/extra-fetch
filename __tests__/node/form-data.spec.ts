03d8bb0035030e8fe81b452ec8f56527b57d641a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-04-27 21:24:39 +0000

    fix: formdata-node

---
+++ __tests__/node/form-data.spec.ts
@@ -0,0 +1,5 @@
+import { FormData } from '@src/form-data'
+
+test('FormData', () => {
+  expect(FormData).not.toBeUndefined()
+})
--- src/form-data.ts
+++ src/form-data.ts
@@ -1,3 +1,3 @@
-import FormDataNode from 'formdata-node'
+import { FormData as FormDataNode } from 'formdata-node'
 
 export const FormData = FormDataNode as unknown as typeof globalThis.FormData
