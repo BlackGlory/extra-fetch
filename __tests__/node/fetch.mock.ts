3636b611312f87be78641614d2fed2bd36ef526b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-10-12 16:02:14 +0000

    test: use node environment

--- __tests__/node/fetch.mock.ts
+++ __tests__/node/fetch.mock.ts
@@ -2,14 +2,14 @@ import { setupServer } from 'msw/node'
 import { rest } from 'msw'
 
 export const server = setupServer(
-  rest.get('/', (req, res, ctx) => {
+  rest.get('http://localhost', (req, res, ctx) => {
     return res(
       ctx.status(200)
     , ctx.json(req)
     )
   })
 
-, rest.delete('/', (req, res, ctx) => {
+, rest.delete('http://localhost', (req, res, ctx) => {
     return res(
       ctx.status(200)
     , ctx.json(req)
