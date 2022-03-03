3ca92dcbb6593f6882bd5e2993e2ac6865fb110b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-03-03 05:48:11 +0000

    chore: upgrade dependencies

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,21 +1,11 @@
-import { Level } from 'extra-logger'
+import { Level, stringToLevel } from 'extra-logger'
 import { ValueGetter } from 'value-getter'
 import { Getter } from 'justypes'
 
 const cache = new WeakMap()
 
 export const LEVEL: Getter<Level> = new ValueGetter(env('EXTRA_FETCH_LOG'))
-  .convert(val => {
-    switch (val?.toLowerCase()) {
-      case 'trace': return Level.Trace
-      case 'debug': return Level.Debug
-      case 'info': return Level.Info
-      case 'warn': return Level.Warn
-      case 'error': return Level.Error
-      case 'fatal': return Level.Fatal
-      default: return Level.None
-    }
-  })
+  .convert(val => stringToLevel(val ?? ''))
   .memoize(cache)
   .get()
 
